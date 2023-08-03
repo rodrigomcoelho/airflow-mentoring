446cb6f0247654be7152ada205b045c8b34897c6
Rodrigo Coelho <rodrigo.coelho@hotmail.com.br>
2023-08-03 19:57:07 -0400

    feat: add dag demo2

--- dags/demo002.py
+++ dags/demo002.py
@@ -2,8 +2,8 @@ from datetime import datetime
 from time import sleep
 
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
+
 
 
 def sleeping() -> None:
@@ -11,16 +11,18 @@ def sleeping() -> None:
     sleep(10)
     print("Legal, estou acordado.")
 
+
 def sleeping2() -> None:
     print("Vou dormir por 3 segundos.")
     sleep(3)
     print("Legal, estou acordado.")
 
+
 with DAG(
-    dag_id="demo_002", start_date=datetime(2023, 7, 1), schedule_interval="@daily",
-    default_args={
-        "owner": "rodrigocoelho"
-    }
+    dag_id="demo_002",
+    start_date=datetime(2023, 7, 1),
+    schedule_interval="@daily",
+    default_args={"owner": "rodrigocoelho"},
 ) as dag:
     start = PythonOperator(task_id="start", python_callable=sleeping)
     stop = PythonOperator(task_id="stop", python_callable=sleeping2)
